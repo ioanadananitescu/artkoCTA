9007e9da79999b5a387d8cec3457ba4de571df09
Ioana Dana <ioanadananitescu@gmail.com>
2023-08-17 20:05:40 +0300

    version 170823

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import FirstPageFeed from '@components/FirstPageFeed';
+import ImageFrontPage from '@components/ImageFrontPage';
 
 const Home = () => {
   
@@ -15,7 +16,22 @@ const Home = () => {
          
    <section className="">
     <div className="relative">
-          <Image
+
+      <ImageFrontPage/>
+{/*     <img
+    	sizes="(min-width: 30em) 65em, 100vw"
+
+    	srcset="https://res.cloudinary.com/dlel1msov/image/upload/f_auto,q_70,w_256/v1687341642/artko/bioartsetup.jpg 256w,
+      https://res.cloudinary.com/dlel1msov/image/upload/f_auto,q_70,w_512/v1687341642/artko/bioartsetup.jpg 512w,
+      https://res.cloudinary.com/dlel1msov/image/upload/f_auto,q_70,w_768/v1687341642/artko/bioartsetup.jpg 768w,
+      https://res.cloudinary.com/dlel1msov/image/upload/f_auto,q_70,w_1500/v1687341642/artko/bioartsetup.jpg 1500w,
+      https://res.cloudinary.com/dlel1msov/image/upload/f_auto,q_70,w_1600/v1687341642/artko/bioartsetup.jpg 1600w"
+    	src="https://res.cloudinary.com/dlel1msov/image/upload/w_2000,h_1600/v1687341642/artko/bioartsetup.jpg 1600w"
+    	alt /> */}
+
+
+
+         {/*  <Image
             width={1500}
             height={700}
       className="md:hidden w-screen mx-auto"
@@ -30,11 +46,11 @@ const Home = () => {
             class="hidden md:max-3xl:block mx-auto left-0 right-0 w-screen"
           src="/assets/images/horizontalFeatured.png"
          
-          alt="Every week our curators find art to feature." />
+          alt="Every week our curators find art to feature." /> 
         <Link href="https://shop.artko.ro/featured"
           class="absolute tracking-wide text-4xl md:text-6xl text-black font-bold
          text-center top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">Uncovering art at your fingertips.
-                 <br className="max-md:hidden" /> </Link>
+                 <br className="max-md:hidden" /> </Link> */}
  
     <button type="button" className=" absolute top-1/8 left-1/2 -translate-x-1/2 -translate-y-1/2 amber_btn">
             Explore the gallery
---
+++ components/ImageFrontPage.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from 'react';
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage, lazyload, responsive, placeholder } from "@cloudinary/react";
+
+
+
+
+const ImageFrontPage = () => {
+    const cld = new Cloudinary({
+        cloud: {
+          cloudName: "dlel1msov"
+        }
+      });
+const file="v1680605235/frontpage/HavanaPhotoSetup.webp";
+    const myImage=cld.image(file);
+
+
+  return (
+    <div >
+          <AdvancedImage
+            cldImg={myImage}
+            plugins={[lazyload ('0', 1),responsive(200),placeholder ({mode: 'predominant-color'})]}
+            
+          />
+        </div>
+  )
+}
+
+export default ImageFrontPage
