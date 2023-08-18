8723f0fcc034835a12036ac95069ceec46d7e868
Ioana Dana <ioanadananitescu@gmail.com>
2023-08-18 07:18:05 +0300

    version 1808232

--- components/ImageFrontPage.jsx
+++ components/ImageFrontPage.jsx
@@ -30,7 +30,7 @@ const file="v1687341642/frontpage/Skinsetup.jpg";
           <AdvancedImage
           
             cldImg={myImage}
-            plugins={[responsive(),placeholder ({mode: 'predominant-color'})]}
+            plugins={[lazyload({rootMargin: '10px 20px 10px 30px', threshold: 0.25}),responsive({steps:[800, 1000, 1400]}),placeholder ({mode: 'predominant-color'})]}
             
           />
         </div>
